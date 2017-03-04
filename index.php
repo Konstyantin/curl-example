90d37823aaa9d463f6b1257fd69b1e711d569f2e
kostya <kostya_nagula@mail.ua>
2017-03-04 20:37:13 +0200

    post request with curl

--- index.php
+++ index.php
@@ -64,7 +64,7 @@ curl_close($curl);
 $cookieFile = 'tmp/cookie.txt';
 
 /**
- * Culr init
+ * Curl init
  */
 $curl = curl_init();
 
@@ -111,8 +111,78 @@ curl_close($curl);
 /**
  * Display data
  */
-echo "<pre>";
-print_r($html);
-echo "</pre>";
+//echo "<pre>";
+//print_r($html);
+//echo "</pre>";
 
+
+/* POST */
+
+/**
+ * Execute request
+ *
+ * @param $url
+ * @param null $postData
+ * @param string $cookieFile
+ * @return mixed
+ */
+function request($url, $postData = null , $cookieFile = 'tmp/cookie.txt')
+{
+    /**
+     * Curl init
+     */
+    $curl = curl_init($url);
+
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+
+    /**
+     * Set user agent
+     */
+    curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:51.0) Gecko/20100101 Firefox/51.0');
+
+
+    curl_setopt($curl, CURLOPT_COOKIEJAR, $cookieFile);
+    curl_setopt($curl, CURLOPT_COOKIEFILE, $cookieFile);
+
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+    /**
+     * Check pass post data
+     */
+    if ($postData) {
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $postData);
+    }
+
+    /**
+     * Result executing curl
+     */
+    $html = curl_exec($curl);
+
+    /**
+     * Close CURL
+     */
+    curl_close($curl);
+
+    /**
+     * Return result
+     */
+    return $html;
+}
+
+file_put_contents('tmp/cookie.txt', '');
+
+/**
+ * Login filed data
+ */
+$postData = [
+    'op' => 'login',
+    'dest' => 'https://www.reddit.com/',
+    'user' => 'konstyantin',
+    'passwd' => '123456789q',
+];
+
+$html = request('https://www.reddit.com/post/login', $postData);
+echo $html;
 ?>
